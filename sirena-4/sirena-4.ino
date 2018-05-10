8da9e83d1a17f1b302bb9362846cd6baf07aac29
AdriaTalon17 <adria.talon.14@lacetania.cat>
2018-05-10 10:12:32 +0200

    Sirena
    Programes fent servir un speaker de pc.

---
+++ sirena-4/sirena-4.ino
@@ -0,0 +1,39 @@
+/**********************************************************************************
+**                                                                               **
+**                                  Sirena                                       **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+const int xiulet = 9;        // donar nom al pin 9 de l’Arduino
+const int pot0 = A0;         // donar nom al pin A0 de l’Arduino
+int valPot0;                 // guardar valor del potenciometre  
+
+//********** Setup ****************************************************************
+void setup()
+{
+  pinMode(xiulet, OUTPUT);   // definir el pin 9 com una sortida
+}
+
+//********** Loop *****************************************************************
+void loop()
+{
+  valPot0 = analogRead(pot0);    // llegir valor potenciòmetre 
+
+  tone(xiulet, 350, valPot0);    // xiulet de durada valPot0
+  delay(20);              // esperar valPot0 del xiulet + valPot0 silenci
+
+
+  
+  tone(xiulet, 8000, valPot0);    // xiulet de durada valPot0
+  delay(20);              // esperar valPot0 del xiulet + valPot0 
+}
+
+//********** Funcions *************************************************************
+
+
+
+
